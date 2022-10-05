a24ebb9270aa026c0eabf7e7cf62d132e257c5b5
Dalton Carlile <dlcarlile93@hotmail.com>
2022-10-05 12:28:38 -0700

    commit tree b66016eefafbaa2c15d00dc4df1d7e5fd4d9b362

---
+++ index.js
@@ -0,0 +1,54 @@
+function receivesAFunction(callback){
+    callback();
+
+}
+
+function returnsANamedFunction(){
+    return function name1(){}
+}
+
+
+function returnsAnAnonymousFunction(){
+    return function () {}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function receivesAFunction(cookDinner){
+//     getGroceries();
+//     cookDinner();
+
+// function getGroceries(){
+
+// }
+
+// }
+
+// function returnsANamedFunction(cookDinner){
+//    function getGroceries(){
+
+//    }
+//    function cookDinner(){
+
+//    }
+//     getGroceries();
+//     cookDinner();
+//     return returnsANamedFunction
+// }
+   
+    
+
+// function returnsAnAnonymousFunction(){
+   
+// } 
+//     returnsAnAnonymousFunction(()  => console.log(''));
